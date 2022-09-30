0dfca18cca3786cd6548784448138580724e7f11
Oliver Njiru <olivernjiru@gmail.com>
2022-09-30 20:43:20 +0300

    moved everything to the react app

--- script.js
+++
@@ -1,9 +0,0 @@
-const submit = document.querySelector('Submit');
-const text = document.getElementById('txtarea');
-
-submit.addEventListener('click', (e) => {
-    console.log(text.length);
-})
-
-
-
--- ventome/src/App.js
+++ ventome/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <div class="container">
+        <h1 class="title">vent to me</h1>
+        <textarea name="textarea" id="txtarea" cols="80" rows="10"></textarea>
+        <div class="btn">
+            <button class = "submit" type="submit">submit</button>
+        </div>
+
     </div>
   );
 }
